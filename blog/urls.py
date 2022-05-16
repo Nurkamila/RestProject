725789a74765e849c6f654a888cff32647998db1
Aigerim <maviboncuaika@gmail.com>
2022-05-16 10:25:23 +0600

    serializer/view

--- blog/urls.py
+++ blog/urls.py
@@ -13,9 +13,19 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+
+from post.views import CategoryListView, PostViewSet
+
+router = DefaultRouter()
+router.register('posts', PostViewSet)
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/v1/categories/', CategoryListView.as_view()),
+    path('api/v1/', include(router.urls)),
 ]

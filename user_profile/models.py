49a198ba1d1ac701692f0bfc75e9cbcd54947791
Aigerim <maviboncuaika@gmail.com>
2022-05-17 11:59:22 +0600

    .

--- user_profile/models.py
+++ user_profile/models.py
@@ -1,10 +1,10 @@
 from django.db import models
 from account.models import MyUser
 
-class Profile(models.Model):
-    user = models.OneToOneField(MyUser, on_delete=models.CASCADE)
-    image = models.ImageField(upload_to='profile_images', blank=True, null = True)
-    bio = models.TextField(blank=True)
-
-    def __str__(self):
-        return self.user.email
+# class Profile(models.Model):
+#     user = models.OneToOneField(MyUser, on_delete=models.CASCADE)
+#     image = models.ImageField(upload_to='profile_images', blank=True, null = True)
+#     bio = models.TextField(blank=True)
+#
+#     def __str__(self):
+#         return self.user.email
